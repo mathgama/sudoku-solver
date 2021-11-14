300cca6b5f2e8f09b4e7fea30ef2385a1241a9df
mathgama <matheus_gama@live.com>
2021-11-14 19:09:57 -0300

    feat: cell selection

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ class Grid:
     self.cell_size = (self.width - (2 * self.padding)) / 9
     self.initial_board = requests.get('https://sugoku.herokuapp.com/board?difficulty=easy').json()['board']
     self.board_state = [[self.initial_board[i][j] for j in range(9)] for i in range(9)]
+    self.selected_cell = None
     #self.cubes = [[Cube(self.board[i][j], i, j) for j in range(9)] for i in range(9)]
 
   def draw_empty_grid(self):
@@ -43,6 +44,9 @@ class Grid:
 
     value_render = font.render(str(value), True, color)
 
+    cell_padding_left = self.cell_size * 21 / 60
+    cell_padding_top = self.cell_size * 13 / 60
+
     self.window.blit(value_render,
                      (self.padding + 15 + (self.cell_size * col),
                       self.padding + 10 + (self.cell_size * row)))
@@ -57,9 +61,31 @@ class Grid:
         if value != 0:
           self.render_number(value, i, j, user_input)
 
+  def highlight_selected_cell(self):
+    if self.selected_cell == None:
+      return
+
+    left = self.padding + (self.selected_cell[1] * self.cell_size) + 1
+    top = self.padding + (self.selected_cell[0] * self.cell_size) + 1
+
+    pygame.draw.rect(self.window,
+                     (255, 0, 0),
+                     (left, top, self.cell_size, self.cell_size),
+                     3)
+
   def draw(self):
     self.draw_empty_grid()
     self.draw_board_state()
+    self.highlight_selected_cell()
+
+  def select_cell(self, mouse_position):
+    row = int((mouse_position[1] - self.padding) // self.cell_size)
+    col = int((mouse_position[0] - self.padding) // self.cell_size)
+
+    if (row, col) == self.selected_cell:
+      self.selected_cell = None
+    elif self.initial_board[row][col] == 0:
+      self.selected_cell = (row, col)
 
 
 """ class Cube:
@@ -73,12 +99,11 @@ def main():
   pygame.init()
   pygame.display.set_caption('Sudoku Game')
 
-  window = pygame.display.set_mode((550, 550))
-  window.fill(background_color)
-
+  window = pygame.display.set_mode((590, 590))
   grid = Grid(25, window)
-  grid.draw()
 
+  window.fill(background_color)
+  grid.draw()
   pygame.display.update()
 
   while True:
@@ -86,5 +111,11 @@ def main():
       if event.type == pygame.QUIT:
         pygame.quit()
         return
+      if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+        grid.select_cell(pygame.mouse.get_pos())
+
+      window.fill(background_color)
+      grid.draw()
+      pygame.display.update()
 
 main()
