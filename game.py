590585e95c4f5ef7ed01d06994466e0f80861350
mathgama <matheus_gama@live.com>
2021-11-14 18:18:22 -0300

    refactor:  using classes

--- game.py
+++ game.py
@@ -3,56 +3,81 @@ import requests
 
 background_color = (255, 255, 255)
 original_element_color = (90, 100, 249)
+user_input_color = (0, 0, 0)
 
-padding = 25
-cell_size = 50
-width = (2 * padding) + (cell_size * 9)
+class Grid:
+  def __init__(self, padding, window):
+    self.window = window
+    self.width = window.get_size()[0]
+    self.padding = padding
+    self.cell_size = (self.width - (2 * self.padding)) / 9
+    self.initial_board = requests.get('https://sugoku.herokuapp.com/board?difficulty=easy').json()['board']
+    self.board_state = [[self.initial_board[i][j] for j in range(9)] for i in range(9)]
+    #self.cubes = [[Cube(self.board[i][j], i, j) for j in range(9)] for i in range(9)]
 
-initial_board_state = requests.get('https://sugoku.herokuapp.com/board?difficulty=easy').json()['board']
+  def draw_empty_grid(self):
+    for i in range(10):
+      if i % 3 == 0:
+        line_thickness = 4
+      else:
+        line_thickness = 2
 
-def draw_empty_grid(window):
-  for i in range(0,10):
-    if(i%3 == 0):
-      line_thickness = 4
-    else:
-      line_thickness = 2
+      # Vertical lines
+      pygame.draw.line(self.window, 
+                      (0,0,0), 
+                      (self.padding + self.cell_size * i, self.padding),
+                      (self.padding + self.cell_size * i, self.width - self.padding), 
+                      line_thickness)
 
-    # Vertical lines
-    pygame.draw.line(window, 
-                     (0,0,0), 
-                     (padding + cell_size * i, padding),
-                     (padding + cell_size * i, width - padding), 
-                     line_thickness)
+      # Horizontal lines
+      pygame.draw.line(self.window, 
+                      (0,0,0), 
+                      (self.padding, self.padding + self.cell_size * i), 
+                      (self.width - self.padding, self.padding + self.cell_size * i), 
+                      line_thickness)
 
-    # Horizontal lines
-    pygame.draw.line(window, 
-                     (0,0,0), 
-                     (padding, padding + cell_size * i), 
-                     (width - padding, padding + cell_size * i), 
-                     line_thickness)
+  def render_number(self, value, row, col, user_input):
+    font = pygame.font.Font(pygame.font.get_default_font(), 40)
 
-def draw_initial_board_state(window):
-  font = pygame.font.Font(pygame.font.get_default_font(), 40)
+    color = user_input_color if user_input == True else original_element_color
 
-  for i in range(0,9):
-    for j in range(0,9):
-      value = initial_board_state[i][j]
+    value_render = font.render(str(value), True, color)
 
-      if(value != 0):
-        value_render = font.render(str(value), True, original_element_color)
-        window.blit(value_render, 
-                    (padding + 15 + (cell_size * j), #40 
-                    padding + 10 + (cell_size * i))) #35
+    self.window.blit(value_render,
+                     (self.padding + 15 + (self.cell_size * col),
+                      self.padding + 10 + (self.cell_size * row)))
+
+  def draw_board_state(self):
+    for i in range(9):
+      for j in range(9):
+        value = self.board_state[i][j]
+
+        user_input = True if self.initial_board[i][j] == 0 else False
+
+        if value != 0:
+          self.render_number(value, i, j, user_input)
+
+  def draw(self):
+    self.draw_empty_grid()
+    self.draw_board_state()
+
+
+""" class Cube:
+  def __init__(self, value, row, col):
+    self.value = value
+    self.row = row
+    self.col = col """
+
 
 def main():
   pygame.init()
   pygame.display.set_caption('Sudoku Game')
 
-  window = pygame.display.set_mode((width, width))
+  window = pygame.display.set_mode((550, 550))
   window.fill(background_color)
 
-  draw_empty_grid(window)
-  draw_initial_board_state(window)
+  grid = Grid(25, window)
+  grid.draw()
 
   pygame.display.update()
 
