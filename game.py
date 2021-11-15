9a08c96114027cc4cbe08bdfec8da5630da6b9a8
mathgama <matheus_gama@live.com>
2021-11-15 15:34:42 -0300

    feat: number input validation

--- game.py
+++ game.py
@@ -1,9 +1,11 @@
 import pygame
+from pygame.display import update
 import requests
 
 background_color = (255, 255, 255)
 original_element_color = (90, 100, 249)
 user_input_color = (0, 0, 0)
+error_input_color = (255, 0, 0)
 
 class Grid:
   def __init__(self, padding, window):
@@ -37,11 +39,49 @@ class Grid:
                       (self.width - self.padding, self.padding + self.cell_size * i), 
                       line_thickness)
 
+  def is_input_valid(self, value, row, col):
+    #print('board', self.board_state)
+
+    # check for the same row
+    for j in range(9):
+      if j != col and self.board_state[row][j] == value:
+        #print('row', row, 'col', j, 'value', self.board_state[row][j])
+        return False
+
+    #print('row ok')
+
+    # check for the same column
+    for i in range(9):
+      if i != row and self.board_state[i][col] == value:
+        return False
+
+    #print('col ok')
+
+    # check for the same block
+    block_x_start = (row // 3) * 3
+    block_y_start = (col // 3) * 3
+
+    for i in range(block_x_start, block_x_start + 2):
+      for j in range(block_y_start, block_y_start + 2):
+        if (i, j) != (row, col) and self.board_state[i][j] == value:
+          return False
+
+    #print('block ok')
+
+    return True
+
   def render_number(self, value, row, col, user_input):
     font_size = int(self.cell_size * 40 // 60)
     font = pygame.font.Font(pygame.font.get_default_font(), font_size)
 
-    color = user_input_color if user_input == True else original_element_color
+    if not user_input:
+      color = original_element_color
+    elif self.is_input_valid(value, row, col):
+      color = user_input_color
+    else:
+      color = error_input_color
+
+    #color = user_input_color if user_input == True else original_element_color
 
     value_render = font.render(str(value), True, color)
 
@@ -114,22 +154,29 @@ def main():
   pygame.display.update()
 
   while True:
+    update_view = False
+
     for event in pygame.event.get():
       if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
         grid.select_cell(pygame.mouse.get_pos())
+        update_view = True
       if event.type == pygame.KEYDOWN:
+        print(event.key)
+
         if event.key == 8 or event.key == 127:
           value = 0
         else:
           value = event.key - 48
 
         grid.type(value)
+        update_view = True
       if event.type == pygame.QUIT:
         pygame.quit()
         return
 
-      window.fill(background_color)
-      grid.draw()
-      pygame.display.update()
+      if update_view:
+        window.fill(background_color)
+        grid.draw()
+        pygame.display.update()
 
 main()
